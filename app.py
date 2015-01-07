dc65fc5e26faa4b1e75798331eb5a8c424f9114f
paskao <paskao@hacari.org>
2015-01-07 14:34:40 +0100

    download table

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+import re
+import requests
 
 from discovery import lang_dirs
 
@@ -30,3 +32,33 @@ def htaccess():
 def render(lang, title):
     template = "%s/%s.html" % (lang, title)
     return render_template(template)
+
+
+def get_images_data():
+    base_url = 'http://download.freepto.mx/latest/'
+
+    latest = requests.get(base_url)
+    if latest.status_code != 200:
+	print 'error'
+        raise Exception('Can\'t download http://download.freepto.mx/latest/')
+
+    latest_text = latest.text.replace('\n', '')
+    locales = re.findall('(freepto-[a-z]{2}_[A-Z]{2}_[\-0-9\.]*)', latest_text)
+
+    images_data = {}
+    for locale in locales:
+        sha512_url = '%s/%s/%s.sha512sum.txt' % (base_url, locale, locale)
+        sha512 = requests.get(sha512_url)
+        if sha512.status_code != 200:
+            print 'error2'
+            raise Exception('Can\'t download %s' % sha512_url)
+
+        images_data[locale] = {
+            'http_download': '%s%s/%s.img' % (base_url, locale, locale),
+            'torrent_download': '%s%s/%s.torrent' % (base_url, locale, locale),
+            'sha512': sha512.text,
+            'sha512sig': sha512_url + '.sig'
+        }
+    return images_data
+
+app.jinja_env.globals.update(get_images_data=get_images_data)
